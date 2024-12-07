414a772201ddacd301263d44018f2c010dbe2c5e
zzdnb <1955093734@qq.com>
2024-12-08 07:54:14 +0800

    remove: entrypoint

--- backend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8000
-exec "$@"
--- frontend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# 启动开发服务器
-npm run dev -- --host 0.0.0.0
-exec "$@"
